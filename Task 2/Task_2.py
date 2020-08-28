74b67e6f786980efcbb189c720bd406b9372e486
SUMIT RANJAN <68730471+Sumit2514@users.noreply.github.com>
2020-08-28 20:55:47 +0530

    Add files via upload

---
+++ Task 2/Task_2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Importing important libraries for this assignment
+
+# In[1]:
+
+
+import slate3k as slate
+import numpy as np
+import pandas as pd
+
+
+# ## Initializtion the variable and creating empty list 
+
+# In[2]:
+
+
+i=4
+arr=[]
+
+
+# ## loop for opening multiple PDF profiles and append to list
+
+# In[3]:
+
+
+while(i<=53):    
+    text=slate.PDF(open('AI_CHAMP/TASK 1/Profile ('+str(i)+').pdf', 'rb')).text()
+    text
+    i=i+1
+    arr=np.append(arr,text)
+        
+## Converting list to DataFrame        
+arr=pd.DataFrame(arr)
+
+
+# ## Printing command for DataFrame
+
+# In[4]:
+
+
+print(arr)
+
+
+# ## Saving DataFrame in CSV 
+
+# In[5]:
+
+
+arr.to_csv (r'C:\Users\Sumit Ranjan\Downloads\Profile_text.csv' ,index = False, header=True)
+
+
+# In[ ]:
+
+
+
+
